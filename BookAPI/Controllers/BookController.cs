e255d01928ae255af63dbfb07254e56261f628ca
adcao0128 <adc135@case.edu>
2024-05-20 10:35:48 -0400

    Add project files.

---
+++ BookAPI/Controllers/BookController.cs
@@ -0,0 +1,63 @@
+﻿using BookAPI;
+using BookAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly DataContext _context;
+
+        public BookController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> getBooks()
+        {
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Book>>> CreateBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Book>>> UpdateBook(Book book)
+        {
+            var dbBook = await _context.Books.FindAsync(book.Id);
+            if (dbBook == null)
+                return BadRequest("Book not Found");
+
+            dbBook.Title = book.Title;
+            dbBook.Description = book.Description;
+            dbBook.Author = book.Author;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Book>>> DeleteBook(int id)
+        {
+            var dbBook = await _context.Books.FindAsync(id);
+            if (dbBook == null)
+                return BadRequest("Book not Found");
+
+            _context.Books.Remove(dbBook);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+    }
+}
---
+++ BookAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Book> Books => Set<Book>();
+    }
+}
